ffb3db7a4d3c6ce285039763f7bbf55f00866d44
laba3 <laba3@gmail.com>
2020-04-21 16:55:24 +0300

    First up-date

---
+++ laba3.1.1/laba3.1.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace laba3._1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input string: ");
+            string line;
+            line = Console.ReadLine();
+            string[] words = line.Split(new[] { " " }, StringSplitOptions.None);
+            string word = "";
+            int ctr = 0;
+            foreach (String s in words)
+            {
+                if (s.Length > ctr)
+                {
+                    word = s;
+                    ctr = s.Length;
+                }
+            }
+
+            Console.WriteLine(word);
+
+        }
+    }
+}
---
+++ laba3.2.2/laba3.2.2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Text;
+namespace laba3._2._2
+{
+    class Test
+    {
+        public static void Main()
+        {
+            string path = @"C:\Users\yuras\Desktop\2-Семестр\oop\laba3\laba3.2\out.txt";
+
+            // This text is added only once to the file.
+            if (!File.Exists(path))
+            {
+                // Create a file to write to.
+                string createText = "Hello and Welcome" + Environment.NewLine;
+                File.WriteAllText(path, createText);
+            }
+
+            string str;
+            string text = ""; //Рядки для розбору виразу      
+            string[] arrStr = new string[2];
+            Console.WriteLine("input string");
+            str = Console.ReadLine() + Environment.NewLine;
+            string[] split = str.Split(new Char[] { '!' });
+            int i = 0;
+            foreach (string s in split)
+            {
+                if (s.Trim() != "")
+                {
+
+                    Console.WriteLine(s);
+                    arrStr[i] = s;
+                    i++;
+                    text = s;
+                }
+            }
+            File.AppendAllText(path, text);
+            // Open the file to read from.
+            string readText = File.ReadAllText(path);
+            Console.WriteLine(readText);
+        }
+    }
+}
+
+         
